bb8d9ef221298cb43dd32a3ca75be2259aab3060
majijdshoorabi <majidshoorabi@gmail.com>
2022-08-06 13:13:49 +0430

    feat - add commandLineRunner and applicationRunner to TacoCloudApplication.java

--- src/main/java/com/github/majidshoorabi/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/github/majidshoorabi/tacocloud/TacoCloudApplication.java
@@ -1,13 +1,30 @@
 package com.github.majidshoorabi.tacocloud;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            // command that run when Application started
+        };
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(){
+        return args -> {
+            // command that run when Application started
+        };
     }
 
 }
